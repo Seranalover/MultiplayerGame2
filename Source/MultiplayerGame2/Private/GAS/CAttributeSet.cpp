9c230644b5e710cd564c9f30d9ff93768629cbee
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-01-30 23:08:46 +0800

    Sever apply GE replicate attributes to Client

--- Source/MultiplayerGame2/Private/GAS/CAttributeSet.cpp
+++ Source/MultiplayerGame2/Private/GAS/CAttributeSet.cpp
@@ -2,4 +2,36 @@
 
 
 #include "GAS/CAttributeSet.h"
+#include "Net/UnrealNetwork.h"
+
+void UCAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	//指定向客户端复制属性的方式
+	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UCAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	//通知GAS组件，属性已发生变化，这会触发一系列事件，比如订阅属性变更的委托会被激活
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCAttributeSet, Health, OldValue);
+}
+
+void UCAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCAttributeSet, MaxHealth, OldValue);
+}
+
+void UCAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCAttributeSet, Mana, OldValue);
+}
+
+void UCAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCAttributeSet, MaxMana, OldValue);
+}
 
--- Source/MultiplayerGame2/Private/GAS/CAttributeSet.h
+++ Source/MultiplayerGame2/Private/GAS/CAttributeSet.h
@@ -27,14 +27,27 @@ public:
 	ATTRIBUTE_ACCESSORS(UCAttributeSet, MaxHealth);
 	ATTRIBUTE_ACCESSORS(UCAttributeSet, Mana);
 	ATTRIBUTE_ACCESSORS(UCAttributeSet, MaxMana);
+	//指定向客户端复制属性的方式
+	virtual void GetLifetimeReplicatedProps( TArray< class FLifetimeProperty > & OutLifetimeProps ) const override;
+
 	
 private:
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_Health)
 	FGameplayAttributeData Health;
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_MaxHealth)
 	FGameplayAttributeData MaxHealth;
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_Mana)
 	FGameplayAttributeData Mana;
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_MaxMana)
 	FGameplayAttributeData MaxMana;
+	
+	//函数被调用时，服务端向客户端拷贝新的数据，且仍会提供旧的数据供你使用，这样新值旧值都可以使用
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
 };
