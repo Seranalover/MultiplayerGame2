730cf5811d67e4f0c6e89aedefd713208af3a002
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-02-02 22:17:00 +0800

    clamp the attributes

--- Source/MultiplayerGame2/Private/GAS/CAttributeSet.cpp
+++ Source/MultiplayerGame2/Private/GAS/CAttributeSet.cpp
@@ -3,6 +3,7 @@
 
 #include "GAS/CAttributeSet.h"
 #include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
 
 void UCAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
 {
@@ -12,6 +13,22 @@ void UCAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>&
 	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UCAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UCAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	if (Attribute == GetHealthAttribute())
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth()); //限制health最大值和最小值
+	if (Attribute == GetManaAttribute())
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana()); //限制mana最大值和最小值
+}
+
+void UCAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth())); //限制health最大值和最小值
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana())); //限制mana最大值和最小值
 }
 
 void UCAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
--- Source/MultiplayerGame2/Private/GAS/CAttributeSet.h
+++ Source/MultiplayerGame2/Private/GAS/CAttributeSet.h
@@ -29,6 +29,21 @@ public:
 	ATTRIBUTE_ACCESSORS(UCAttributeSet, MaxMana);
 	//指定向客户端复制属性的方式
 	virtual void GetLifetimeReplicatedProps( TArray< class FLifetimeProperty > & OutLifetimeProps ) const override;
+	
+	/**
+		 *	Called just before any modification happens to an attribute. This is lower level than PreAttributeModify/PostAttribute modify.
+		 *	There is no additional context provided here since anything can trigger this. Executed effects, duration based effects, effects being removed, immunity being applied, stacking rules changing, etc.
+		 *	This function is meant to enforce things like "Health = Clamp(Health, 0, MaxHealth)" and NOT things like "trigger this extra thing if damage is applied, etc".
+		 *	
+		 *	NewValue is a mutable reference so you are able to clamp the newly applied value as well.
+		 */
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override; //限制属性的最大值和最小值
+	
+	/**
+	 *	Called just after a GameplayEffect is executed to modify the base value of an attribute. No more changes can be made.
+	 *	Note this is only called during an 'execute'. E.g., a modification to the 'base value' of an attribute. It is not called during an application of a GameplayEffect, such as a 5 second +10 movement speed buff.
+	 */
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData &Data) override; //限制属性的最大值和最小值，与上一个函数的区别是调用时机不同
 
 	
 private:
