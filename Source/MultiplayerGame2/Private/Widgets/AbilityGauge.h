36882dac1da711aaf64d7db222ecd820d7a12458
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-02-09 15:52:16 +0800

    create Ability Gauge class

---
+++ Source/MultiplayerGame2/Private/Widgets/AbilityGauge.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityGauge.generated.h"
+
+/**
+ * 技能图标UI
+ */
+UCLASS()
+class UAbilityGauge : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	class UImage* Icon;
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CooldownCounterText;
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CooldownDurationText;
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* CostText;
+};
