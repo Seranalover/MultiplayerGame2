2f83592f9673ce576e8fba5320cb173189bd8e07
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-02-11 15:31:09 +0800

    add the GroundBlast Ability

---
+++ Source/MultiplayerGame2/Private/GAS/GA_GroundBlast.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/CGameplayAbility.h"
+#include "GA_GroundBlast.generated.h"
+
+/**
+ * 地面爆破技能类
+ */
+UCLASS()
+class UGA_GroundBlast : public UCGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	/** Actually activate ability, do not call this directly */
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Aimation")
+	UAnimMontage* GroundBlastMontage;
+};
