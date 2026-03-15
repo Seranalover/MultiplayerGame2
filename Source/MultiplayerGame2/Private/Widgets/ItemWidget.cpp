b4797ea5cfc171f159a8bb3e77f18fb159fdf0b6
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-03-15 15:16:08 +0800

    create and test the Item Widget Class

---
+++ Source/MultiplayerGame2/Private/Widgets/ItemWidget.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/ItemWidget.h"
+
+#include "Components/Image.h"
+
+void UItemWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	SetIsFocusable(true); //不设置可聚焦，处理鼠标左右键点击时会产生错误
+}
+
+void UItemWidget::SetIcon(UTexture2D* IconTexture)
+{
+	ItemIcon->SetBrushFromTexture(IconTexture);
+}
+
+FReply UItemWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	FReply SuperReply = Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+	if (InMouseEvent.IsMouseButtonDown(EKeys::RightMouseButton))
+	{
+		return FReply::Handled().SetUserFocus(TakeWidget());
+	}
+	if (InMouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton))
+	{
+		return FReply::Handled().SetUserFocus(TakeWidget()).DetectDrag(TakeWidget(), EKeys::LeftMouseButton); //左键能响应拖拽事件
+	}
+	return SuperReply;
+}
+
+FReply UItemWidget::NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	FReply SuperReply = Super::NativeOnMouseButtonUp(InGeometry, InMouseEvent);
+	if (HasAnyUserFocus())
+	{
+		if (InMouseEvent.GetEffectingButton() == EKeys::RightMouseButton)
+		{
+			RightButtonClicked();
+			return FReply::Handled();
+		}
+		if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+		{
+			LeftButtonClicked();
+			return FReply::Handled();
+		}
+	}
+	return SuperReply;
+}
+
+void UItemWidget::RightButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Right Button Clicked"));
+}
+
+void UItemWidget::LeftButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Left Button Clicked"));
+}
---
+++ Source/MultiplayerGame2/Private/Widgets/ItemWidget.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemWidget.generated.h"
+
+/**
+ * 物品控件类
+ * 处理物品公用功能，如设置图标，处理右键点击事件等
+ */
+UCLASS()
+class UItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	virtual void SetIcon(UTexture2D* IconTexture); //这里设置为virtual，方便以后子类重写方法，实现使用装备主动技能时，图标显示倒计时等功能
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	class UImage* ItemIcon;
+	
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override; //鼠标按下
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override; //鼠标释放
+	virtual void RightButtonClicked(); //点击右键
+	virtual void LeftButtonClicked(); //点击左键
+};
