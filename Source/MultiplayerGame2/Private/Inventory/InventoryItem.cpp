9613f5824311076cedb46408805f876afb714251
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-03-16 20:33:42 +0800

    create the Inventory Item Handle class

--- Source/MultiplayerGame2/Private/Inventory/InventoryItem.cpp
+++ Source/MultiplayerGame2/Private/Inventory/InventoryItem.cpp
@@ -3,3 +3,49 @@
 
 #include "Inventory/InventoryItem.h"
 
+FInventoryItemHandle::FInventoryItemHandle()
+	:HandleId(GetInvalidId()) //确保只能通过工厂函数来获得有效的实例
+{
+}
+
+FInventoryItemHandle::FInventoryItemHandle(uint32 HandleId)
+	:HandleId(HandleId)
+{
+}
+
+FInventoryItemHandle FInventoryItemHandle::InvalidHandle()
+{
+	static FInventoryItemHandle InvalidHandle = FInventoryItemHandle();
+	return InvalidHandle;
+}
+
+FInventoryItemHandle FInventoryItemHandle::CreateHandle()
+{
+	return FInventoryItemHandle(GenerateNextId());
+}
+
+bool FInventoryItemHandle::IsValid() const
+{
+	return HandleId != GetInvalidId();
+}
+
+uint32 FInventoryItemHandle::GenerateNextId()
+{
+	static uint32 StaticId = 1;
+	return StaticId++;
+}
+
+uint32 FInventoryItemHandle::GetInvalidId()
+{
+	return 0;
+}
+
+bool operator==(const FInventoryItemHandle& Lhs, const FInventoryItemHandle& Rhs)
+{
+	return Lhs.GetHandleId() == Rhs.GetHandleId();
+}
+
+uint32 GetTypeHash(const FInventoryItemHandle& Key)
+{
+	return Key.GetHandleId();
+}
--- Source/MultiplayerGame2/Private/Inventory/InventoryItem.h
+++ Source/MultiplayerGame2/Private/Inventory/InventoryItem.h
@@ -7,8 +7,37 @@
 #include "InventoryItem.generated.h"
 
 /**
+ * 句柄结构体
+ */
+USTRUCT()
+struct FInventoryItemHandle
+{
+	GENERATED_BODY()
+public:
+	FInventoryItemHandle(); //禁用公共构造函数
+	static FInventoryItemHandle InvalidHandle(); //无效实例，验证用
+	static FInventoryItemHandle CreateHandle(); //工厂函数，调用私有构造函数，生成唯一实例
+	bool IsValid() const; //是否有效实例
+	uint32 GetHandleId() const { return HandleId; } //获得句柄id
+private:
+	UPROPERTY()
+	uint32 HandleId;
+	
+	explicit FInventoryItemHandle(uint32 HandleId); //仅使用私有构造函数
+	static uint32 GenerateNextId(); //生成唯一id
+	static uint32 GetInvalidId(); //无效id标识
+};
+
+bool operator == (const FInventoryItemHandle& Lhs, const FInventoryItemHandle& Rhs);
+uint32 GetTypeHash(const FInventoryItemHandle& Key); //物品类型哈希映射
+
+/**
  * 商店物品实例类
  * 仅保留对物品定义的指针
+ * 
+ * 存储物品原子数据、账户数据信息、授予的能力、和对应能力的句柄、效果器、和对应的效果器句柄，以及其他的必要数据信息
+ * 意味着该类会非常庞大，如果通过网络直接传输该类的副本，效率很低
+ * 因此通过传输inventory item handle句柄，作为唯一标识符来识别特定物品，提高效率，双方会通过该标识符创建各自的副本数据
  */
 UCLASS()
 class UInventoryItem : public UObject
