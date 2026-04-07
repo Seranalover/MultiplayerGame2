44be62b1d543362b1705303785fd9442caf994b2
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-04-08 03:01:44 +0800

    create the RenderActor class

---
+++ Source/MultiplayerGame2/Private/Widgets/RenderActor.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/RenderActor.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+
+// Sets default values
+ARenderActor::ARenderActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	RootComp = CreateDefaultSubobject<USceneComponent>("Root Comp");
+	SetRootComponent(RootComp);
+	
+	CaptureComponent = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture Component");
+	CaptureComponent->SetupAttachment(RootComp);
+	
+	CaptureComponent->bCaptureEveryFrame = false; //关闭每帧渲染，开销很大，因为实际上是在渲染另一个viewport
+	CaptureComponent->FOVAngle = 30.f; //避免头部拉伸
+}
+
+void ARenderActor::SetRenderTarget(class UTextureRenderTarget2D* RenderTarget)
+{
+	CaptureComponent->TextureTarget = RenderTarget;
+}
+
+void ARenderActor::UpdateRenderTarget()
+{
+	if (CaptureComponent)
+		CaptureComponent->CaptureScene();
+}
+
+// Called when the game starts or when spawned
+void ARenderActor::BeginPlay()
+{
+	Super::BeginPlay();
+	CaptureComponent->ShowOnlyActorComponents(this); //使Capture Comp只渲染当前actor，忽视环境中的其他因素
+}
+
---
+++ Source/MultiplayerGame2/Private/Widgets/RenderActor.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RenderActor.generated.h"
+
+/**
+ * 特写渲染类
+ */
+UCLASS()
+class ARenderActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARenderActor();
+	void SetRenderTarget(class UTextureRenderTarget2D* RenderTarget); //设置渲染目标
+	void UpdateRenderTarget(); //更新渲染
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category="Render Actor")
+	class USceneComponent* RootComp;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category="Render Actor")
+	class USceneCaptureComponent2D* CaptureComponent;
+
+};
