dca93635742a16ab082b1e8a9fdb689152093f90
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-04-25 07:00:22 +0800

    setup the Montage play and Mana Cosuption logic of the Lazer Ability

---
+++ Source/MultiplayerGame2/Private/GAS/GA_Lazer.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GA_Lazer.h"
+
+#include "AbilitySystemComponent.h"
+#include "CAttributeSet.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Abilities/Tasks/AbilityTask_WaitCancel.h"
+
+void UGA_Lazer::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!K2_CommitAbility() || !LazerMontage)
+	{
+		K2_EndAbility();
+		return;
+	}
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		UAbilityTask_PlayMontageAndWait* PlayLazerMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, LazerMontage);
+		PlayLazerMontageTask->OnBlendOut.AddDynamic(this, &UGA_Lazer::K2_EndAbility);
+		PlayLazerMontageTask->OnCancelled.AddDynamic(this, &UGA_Lazer::K2_EndAbility);
+		PlayLazerMontageTask->OnInterrupted.AddDynamic(this, &UGA_Lazer::K2_EndAbility);
+		PlayLazerMontageTask->OnCompleted.AddDynamic(this, &UGA_Lazer::K2_EndAbility);
+		PlayLazerMontageTask->ReadyForActivation();
+		
+		UAbilityTask_WaitGameplayEvent* WaitShotEvent = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, GetShootTag());
+		WaitShotEvent->EventReceived.AddDynamic(this, &UGA_Lazer::ShootLazer);
+		WaitShotEvent->ReadyForActivation();
+		
+		UAbilityTask_WaitCancel* WaitCancel = UAbilityTask_WaitCancel::WaitCancel(this);
+		WaitCancel->OnCancel.AddDynamic(this, &UGA_Lazer::K2_EndAbility);
+		WaitCancel->ReadyForActivation();
+	}
+}
+
+void UGA_Lazer::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	UAbilitySystemComponent* OwnerASC = GetAbilitySystemComponentFromActorInfo();
+	if (OwnerASC && OnGoingConsumtionEffectHandle.IsValid())
+	{
+		OwnerASC->RemoveActiveGameplayEffect(OnGoingConsumtionEffectHandle);
+	}
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+FGameplayTag UGA_Lazer::GetShootTag()
+{
+	return FGameplayTag::RequestGameplayTag("ability.lazer.shoot");
+}
+
+void UGA_Lazer::ShootLazer(FGameplayEventData Payload)
+{
+	if (K2_HasAuthority())
+	{
+		OnGoingConsumtionEffectHandle = BP_ApplyGameplayEffectToOwner(OnGoingConsumtionEffect, GetAbilityLevel(CurrentSpecHandle, CurrentActorInfo));
+		UAbilitySystemComponent* OwnerASC = GetAbilitySystemComponentFromActorInfo();
+		if (OwnerASC)
+		{
+			OwnerASC->GetGameplayAttributeValueChangeDelegate(UCAttributeSet::GetManaAttribute()).AddUObject(this, &UGA_Lazer::ManaUpdated);
+		}
+	}
+}
+
+void UGA_Lazer::ManaUpdated(const FOnAttributeChangeData& ChangeData)
+{
+	UAbilitySystemComponent* OwnerASC = GetAbilitySystemComponentFromActorInfo();
+	if (OwnerASC && !OwnerASC->CanApplyAttributeModifiers(OnGoingConsumtionEffect.GetDefaultObject(), 
+		GetAbilityLevel(CurrentSpecHandle, CurrentActorInfo), MakeEffectContext(CurrentSpecHandle, CurrentActorInfo))) //CanApplyAttributeModifiers()判断是否允许拉取游戏效果，同时属性不能低于负值
+	{
+		K2_EndAbility();
+	}
+}
---
+++ Source/MultiplayerGame2/Private/GAS/GA_Lazer.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/CGameplayAbility.h"
+#include "GA_Lazer.generated.h"
+
+/**
+ * 技能 - 激光
+ */
+UCLASS()
+class UGA_Lazer : public UCGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	static FGameplayTag GetShootTag();
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	TSubclassOf<UGameplayEffect> OnGoingConsumtionEffect;
+	
+	FActiveGameplayEffectHandle OnGoingConsumtionEffectHandle;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Anim")
+	class UAnimMontage* LazerMontage;
+	
+	UFUNCTION()
+	void ShootLazer(FGameplayEventData Payload);
+	void ManaUpdated(const FOnAttributeChangeData& ChangeData);
+};
