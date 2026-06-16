ad28e0c50c4b695e10b5fb08a4fa689d2020e561
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-06-16 18:51:43 +0800

    add the Character Defination Primary Asset

---
+++ Source/MultiplayerGame2/Private/Character/PA_CharacterDefination.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PA_CharacterDefination.generated.h"
+
+class ACCharacter;
+/**
+ * 大厅角色数据资产类
+ */
+UCLASS()
+class UPA_CharacterDefination : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
+	static FPrimaryAssetType GetCharacterDefinationAssetType();
+	
+	FString GetCharacterDisplayName() const { return CharacterName; }
+	UTexture2D* LoadIcon() const;
+	TSubclassOf<ACCharacter> LoadCharacterClass() const;
+	TSubclassOf<UAnimInstance> LoadDisplayAnimBP() const;
+	class USkeletalMesh* LoadDisplayMesh() const;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Character")
+	FString CharacterName;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Character")
+	TSoftObjectPtr<UTexture2D> CharacterIcon;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Character")
+	TSoftClassPtr<ACCharacter> CharacterClass;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Character")
+	TSoftClassPtr<UAnimInstance> DisplayAnimBP;
+};
