ec1105d6928bad23d43f742c537d1106971c835a
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-06-29 15:47:12 +0800

    add the Player Selection and Character Selection Server Functions

--- Source/MultiplayerGame2/Private/Player/CPlayerState.cpp
+++ Source/MultiplayerGame2/Private/Player/CPlayerState.cpp
@@ -3,3 +3,55 @@
 
 #include "Player/CPlayerState.h"
 
+#include "Framework/CGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+
+ACPlayerState::ACPlayerState()
+{
+	bReplicates = true;
+	NetUpdateFrequency = 100.f; //该类更新频率很低，因此提升更新频率保证数据实时更新
+}
+
+void ACPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACPlayerState, PlayerSelection);
+}
+
+void ACPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	CGameState = Cast<ACGameState>(UGameplayStatics::GetGameState(this));
+	
+	if (CGameState)
+	{
+		CGameState->OnPlayerSelectionUpdated.AddUObject(this, &ACPlayerState::PlayerSelectionUpdated);
+	}
+}
+
+void ACPlayerState::Server_SetSelectedCharacterDefinition_Implementation(const UPA_CharacterDefination* NewDefinition)
+{
+	if (!CGameState) return;
+	if (!NewDefinition) return;
+	if (CGameState->IsDefinitionSelected(NewDefinition)) return;
+	if (PlayerSelection.GetCharacterDefinition())
+		CGameState->SetCharacterDeselected(PlayerSelection.GetCharacterDefinition());
+	
+	PlayerSelection.SetCharacterDefinition(NewDefinition);
+	CGameState->SetCharacterSelected(this, NewDefinition);
+}
+
+bool ACPlayerState::Server_SetSelectedCharacterDefinition_Validate(const UPA_CharacterDefination* NewDefinition)
+{
+	return true;
+}
+
+void ACPlayerState::PlayerSelectionUpdated(const TArray<FPlayerSelection>& NewPlayerSelections)
+{
+	for (const FPlayerSelection& NewPlayerSelection : NewPlayerSelections)
+	{
+		if (NewPlayerSelection.IsForPlayer(this))
+			PlayerSelection = NewPlayerSelection;
+	}
+}
--- Source/MultiplayerGame2/Private/Player/CPlayerState.h
+++ Source/MultiplayerGame2/Private/Player/CPlayerState.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PlayerInfoTypes.h"
 #include "GameFramework/PlayerState.h"
 #include "CPlayerState.generated.h"
 
@@ -15,4 +16,20 @@ class ACPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 	
+public:
+	ACPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void BeginPlay() override;
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetSelectedCharacterDefinition(const UPA_CharacterDefination* NewDefinition);
+	
+private:
+	UPROPERTY(Replicated)
+	FPlayerSelection PlayerSelection;
+	
+	UPROPERTY()
+	class ACGameState* CGameState;
+	
+	void PlayerSelectionUpdated(const TArray<FPlayerSelection>& NewPlayerSelections);
 };
