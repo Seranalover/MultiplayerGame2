932fdb1ea2f99d47df0bfe8c08252b6c0906b985
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-07-30 17:08:52 +0800

    implement the Map loading in the CGameInstance class

---
+++ Source/MultiplayerGame2/Private/Framework/CGameInstance.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CGameInstance.generated.h"
+
+/**
+ * 游戏实例类
+ * 在游戏运行期间持续存在，伴随整个游戏进程
+ * 可用于在其基础上构建网络功能，涉及到大厅的创建和epic在线服务的连接
+ * 可用于实现地图加载功能
+ */
+UCLASS()
+class UCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	void StartMatch(); //点击 开始游戏 时加载游戏关卡
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> MainMenuLevel; //使用软引用，任何硬引用都会将资源整个加载到内存中
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> LobbyLevel;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> GameLevel;
+	
+	void LoadLevelAndListen(TSoftObjectPtr<UWorld> Level);
+};
