abe039bb59ac84e2fe320c7d3619ff94775dacdd
Seranalover <5608326+seranalover@user.noreply.gitee.com>
2026-08-24 16:05:00 +0800

    process the Port Query Result to get the Occupied Ports

---
+++ ServerDeploy/coordinator/coordinator/coordinator.py
@@ -0,0 +1,56 @@
+# 从flask Lib中引入Flask, request, jsonify组件
+from flask import Flask, request, jsonify
+import subprocess
+from consts import SESSION_NAME_KEY, SESSION_SEARCH_ID_KEY, PORT_KEY
+import re
+
+app=Flask(__name__)
+
+def GetUsedPorts():
+    result = subprocess.run(['docker', 'ps', '--format', '"{{.Ports}}"'], capture_output=True, text=True)
+    output = result.stdout
+    usedPorts = set()
+    for line in output.strip().split("\n"):
+        matches = re.findall(r'0\.0\.0\.0:(\d+)->', line)
+        usedPorts.update(map(int, matches))
+
+    return usedPorts
+
+def CreateServerImpl(sessionName, sessionSearchId):
+    ports = GetUsedPorts()
+    print(ports)
+
+# TODO: remove when using docker in the future
+nextAvailablePort = 7777
+
+# 本地测试方法
+def CreateServerLocalTest(sessionName, sessionSearchId):
+    global nextAvailablePort
+    subprocess.Popen([
+        "C:/Users/Qian/Documents/UnrealSrc/UnrealEngine/Engine/Binaries/Win64/UnrealEditor.exe",
+        "C:/Users/Qian/Documents/Unreal Projects/MultiplayerGame2/MultiplayerGame2.uproject",
+        "-server",
+        "-log",
+        '-epicapp="ServerClient"',
+        f'-SESSION_NAME="{sessionName}"',
+        f'-SESSION_SEARCH_ID="{sessionSearchId}"',
+        f'-PORT={nextAvailablePort}'
+    ])
+
+    usedPort = nextAvailablePort
+    nextAvailablePort += 1
+    return usedPort
+
+@app.route('/Sessions', methods=['POST'])
+def CreateServer():
+    print(dict(request.headers))
+
+    sessionName = request.get_json().get(SESSION_NAME_KEY)
+    sessionSearchId = request.get_json().get(SESSION_SEARCH_ID_KEY)
+
+    port = CreateServerLocalTest(sessionName, sessionSearchId)
+    return jsonify({"status": "success", PORT_KEY: port}), 200
+
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=5000)
+    CreateServerImpl("", "")
